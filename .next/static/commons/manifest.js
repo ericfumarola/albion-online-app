86f4b892357d7cc67fdc45c47685aa91e86c039c
Eric <ericnicolasfumarola@gmail.com>
2018-09-11 18:06:07 -0300

    itemdata recibida en expand

--- .next/static/commons/manifest.js
+++ .next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "7fc81d89dbde3932db82"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "47a6aa1d72698fa6f888"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_app.js","5":"bundles/pages/items.js","7":"bundles/pages/items/farmableitem.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_app.js","4":"bundles/pages/items/farmableitem.js","5":"bundles/pages/items.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- comps/ExpansionPanel.js
+++ comps/ExpansionPanel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
@@ -6,7 +7,48 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import cors from 'cors';
+
+class SimpleExpansionPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+
+        this.state = {
+            itemData: null
+        };
+    }
+
+
+    getItemData = itemId => {
+        let query = `http://localhost:5000/item/${itemId}`;
+
+        axios.get(query)
+            .then((response) => {
+                this.setState({itemData: response.data})
+            })
+    };
+
+    render() {
+        const {classes, itemDescription, itemTitle, itemId} = this.props;
+        const {itemData} = this.state;
+        console.log('itemData', itemData)
+        return (
+            <div className={classes.root}>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>} onClick={() => this.getItemData(itemId)}>
+                        <Typography className={classes.heading}>{itemTitle}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Typography>
+                            {itemDescription}
+                        </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+        );
+    }
+
+}
 
 const styles = theme => ({
     root: {
@@ -18,43 +60,6 @@ const styles = theme => ({
     },
 });
 
-const getItemData = itemId => {
-    let options = {
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin':'*'
-        }
-    };
-    let query = `http://localhost:5000/item/${itemId}`;
-
-    fetch(query, options)
-        .then((response) => {
-            return response.json()
-        })
-        .then((recurso) => {
-            console.log(recurso)
-        });
-
-   return '';
-};
-
-function SimpleExpansionPanel(props) {
-    const { classes } = props;
-    return (
-        <div className={classes.root}>
-            <ExpansionPanel>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} onClick={() => getItemData(props.itemId)}>
-                    <Typography className={classes.heading}>{props.itemTitle}</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Typography>
-                        {props.itemDescription}
-                    </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-        </div>
-    );
-}
 
 SimpleExpansionPanel.propTypes = {
     classes: PropTypes.object.isRequired,
